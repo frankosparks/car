f61070047dcfbeeeac00696b4370dacbe611675e
Frank <mainafrank351@gmail.com>
2024-01-22 17:05:23 +0300

    Initial commit.

---
+++ src/car_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { car_backend } from "../../declarations/car_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await car_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
